9bf994f6a6342c2de90f8dc65727823b7e4b4922
Naveen Kumar Vadla <vnaveenkumar9705@gmail.com>
2021-08-20 14:21:39 +0530

    [naveen] step 2: Added Eval phase
    - added eval phase for symbols
    - added eval phase for arithmetic operations
    - added eval phase for List
    - added eval phase for Vector

--- impls/naveen/step2_eval.js
+++ impls/naveen/step2_eval.js
@@ -1,6 +1,7 @@
 const readline = require('readline');
 const { read_str } = require('./reader');
 const { print_str } = require('./printer');
+const { MalSymbol, List, Vector } = require('./types');
 
 const options = {
   input: process.stdin,
@@ -9,11 +10,42 @@ const options = {
 
 const rl = readline.createInterface(options);
 
+const env = {
+  '+': (a, b) => a + b,
+  '-': (a, b) => a - b,
+  '*': (a, b) => a * b,
+  '/': (a, b) => a / b,
+  '%': (a, b) => a % b,
+  'pi': Math.PI
+};
+
+const eval_ast = (ast, env) => {
+  if(ast instanceof MalSymbol) {
+    const val = env[ast.symbol];
+    if(val) return val;
+    throw new Error(`symbol '${ast.symbol}' not found`);
+  }
+  if(ast instanceof List) return new List(ast.ast.map(x => EVAL(x, env)));
+  if(ast instanceof Vector) return new Vector(ast.ast.map(x => EVAL(x, env)));
+
+  return ast;
+};
+
 const READ = (str) => read_str(str);
-const EVAL = (ast) => ast;
+
+const EVAL = (ast, env) => {
+  if(!(ast instanceof List)) return eval_ast(ast, env);
+  if(ast.isEmpty()) return ast;
+
+  const [fn, ...args] = eval_ast(ast, env).ast;
+  if(!(fn instanceof Function)) throw new Error(`'${fn}' is not a function`);
+
+  return fn.apply(null, args);
+}
+
 const PRINT = (ast) => print_str(ast, true);
 
-const rep = (str) => PRINT(EVAL(READ(str)));
+const rep = (str) => PRINT(EVAL(READ(str), env));
 
 const main = () => {
   rl.question('user> ', (str) => {
