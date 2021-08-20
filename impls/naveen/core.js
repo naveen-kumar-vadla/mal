a9d565b61673c2840787ae42cd25e642b1984ae0
Naveen Kumar Vadla <vnaveenkumar9705@gmail.com>
2021-08-20 22:23:09 +0530

    [naveen] step4: Added ability for 'prn'

--- impls/naveen/core.js
+++ impls/naveen/core.js
@@ -1,5 +1,6 @@
 const { Env } = require('./env');
-const { MalSymbol } = require('./types');
+const { MalSymbol, Nil } = require('./types');
+const { print_str } = require('./printer');
 
 const add = (...args) => {
   if (args.length < 2) args.unshift(0);
@@ -26,6 +27,11 @@ const reminder = (...args) => {
   return args.reduce((a, b) => a % b);
 };
 
+const prn = (...args) => {
+  console.log(args.map(x => print_str(x, true)).join(' '));
+  return Nil;
+}
+
 const coreEnv = new Env();
 
 coreEnv.set(new MalSymbol('+'), add);
@@ -34,5 +40,6 @@ coreEnv.set(new MalSymbol('*'), multiply);
 coreEnv.set(new MalSymbol('/'), divide);
 coreEnv.set(new MalSymbol('%'), reminder);
 coreEnv.set(new MalSymbol('pi'), Math.PI);
+coreEnv.set(new MalSymbol('prn'), prn);
 
 module.exports = { coreEnv };
